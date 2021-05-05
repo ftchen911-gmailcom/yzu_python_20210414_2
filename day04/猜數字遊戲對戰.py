e53737f820a23a106b27ac47301ef04389adf26b
ftchen911_gmailcom <ftchen911@gmail.com>
2021-05-05 20:57:32 +0800

    pp

--- day04/猜數字遊戲對戰.py
+++ day04/猜數字遊戲對戰.py
@@ -2,7 +2,19 @@ import random as r
 ans = r.randint(1,99)
 min = 0
 max = 100
-count = 50
+count = 5
+
+# 判定結果
+def check(guess, who):
+    global min, max
+    if guess > ans:
+        max = guess
+    elif guess < ans:
+        min = guess
+    else:
+        print('%s答對了' % who)
+        return True
+    return False
 
 while count > 0:
     # 使用者猜
@@ -12,26 +24,17 @@ while count > 0:
         print('數字範圍錯誤')
         continue
 
-    # 將 count 減去一次
-    count = count - 1
-
     # 判定結果
-    if guess > ans:
-        max = guess
-    elif guess < ans:
-        min = guess
-    else:
-        print('使用者答對了')
+    if check(guess, '使用者'):
         break
 
     # 電腦猜
     guess = r.randint(min+1, max-1)
+    print('(%d). 電腦猜 %d ~ %d : %d' % (count, min, max, guess))
 
     # 判定結果
-    if guess > ans:
-        max = guess
-    elif guess < ans:
-        min = guess
-    else:
-        print('電腦答對了')
+    if check(guess, '電腦'):
         break
+
+    # 將 count 減去一次
+    count = count - 1
