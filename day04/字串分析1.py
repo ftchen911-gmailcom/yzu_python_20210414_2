4112963177c85c7d39630a23fef05998b8f0fe0e
ftchen911_gmailcom <ftchen911@gmail.com>
2021-05-05 19:41:31 +0800

    pp

--- day04/字串分析1.py
+++ day04/字串分析1.py
@@ -14,3 +14,7 @@ print('有沒有 land 這個字:', '有' if str.find('land') >= 0 else '沒有')
 print('有幾個單字:', str.count(' ')+1)
 #  是否都是英文字(a ~ z, A ~ Z)
 print('是否都是英文字:', str.replace(' ','').isalpha())  # 小技巧: 先利用 replace 將中間的空白去除
+print(str[2])
+print(str[0:3])  # 取出0~3位置的字串(不含3)
+path = r'C:\temp\nba\score.py'  # r 不解釋特殊碼, 例如 \t, \n 等等
+print('路徑位置:', path)
