acdc477b2bc1ec9334219be45bb29c3189aedfc5
ftchen911_gmailcom <ftchen911@gmail.com>
2021-05-05 21:30:05 +0800

    pp

--- day04/迴圈NineTable.py
+++ day04/迴圈NineTable.py
@@ -3,11 +3,11 @@
 # 2*1=2 2*2=4 ... 2*9=18
 # ...
 # 9*1=9 9*2=18 ... 9*9=81
-
-for x in range(1, 10):
-    for y in range(1, 10):
+n = int(input('請輸入乘法表 size:'))
+for x in range(1, n):
+    for y in range(1, n):
         z = x * y
-        print("%02d*%02d=%02d" % (x, y, z), end=" ")  # end 斷行
+        print("%2d*%-2d=%3d" % (x, y, z), end=" ")  # %-2d, 由右往左排, end 斷行
     print()  # 斷行
 
 
