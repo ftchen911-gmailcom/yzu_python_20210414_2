c79176a578313e94179efec3f5b304878b6a1910
ftchen911_gmailcom <ftchen911@gmail.com>
2021-05-12 21:31:12 +0800

    pp

--- day05/DictAndList2.py
+++ day05/DictAndList2.py
@@ -5,12 +5,23 @@ emps = [e1, e2, e3]
 print(len(emps), emps)
 
 # 請求出會 Python 的員工人名, 並將放入到 names = []
-sum = 0
+# sum = 0
+# name = []
+# for emp in emps:
+#     for prg in emp['program']:
+#         if prg == 'Python':
+#             name.append(emp['name'])
+#             print("%s" % emp['name'])
+# print(name)
+
+# 請求出會 Python 的員工人名, 並將放入到 names = []
+target = 'Python'
 name = []
 for emp in emps:
-    for prg in emp['program']:
-        if prg == 'Python':
+    # print(emp['name'], emp['program'])
+    for p in emp['program']:
+        if p == target:
+            # print(emp['name'], p)
             name.append(emp['name'])
-            print("%s" % emp['name'])
 print(name)
 
