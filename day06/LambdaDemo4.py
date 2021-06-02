ea8d582f13e0f13889488e91b89ca4f5fb6189ac
ftche <ftchen911@gmail.com>
2021-06-02 20:58:33 +0800

    python

---
+++ day06/LambdaDemo4.py
@@ -0,0 +1,7 @@
+# 利用 lambda 設計一個 bmi  函數式宣告
+# 並求出 170, 60 的 bmi 值
+
+bmiValue = lambda h, w : w / ((h/100) ** 2)
+printBmiValue = lambda value : print("%.2f" % value)
+printBmiValue(bmiValue(170, 60))
+
