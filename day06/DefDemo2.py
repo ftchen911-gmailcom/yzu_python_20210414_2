887ec671e715ca5fd2db5d38832ba3e6fdb7c0bf
ftche <ftchen911@gmail.com>
2021-06-02 18:53:27 +0800

    ft test

---
+++ day06/DefDemo2.py
@@ -0,0 +1,9 @@
+def get_sum(*score):
+    #print(type(score), score, score[0], len(score))
+    print(type(score), score)
+    return sum(score)
+
+print(get_sum(10, 20, 30))
+print(get_sum(10))
+print(get_sum())
+print(get_sum(10, 3.14))
