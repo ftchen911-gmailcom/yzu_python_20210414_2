9557b611d4d2cfa32bb2324da0952b8249166f06
ftche <ftchen911@gmail.com>
2021-06-16 19:24:16 +0800

    python

--- day08/OO_2.py
+++ day08/OO_2.py
@@ -1,18 +1,34 @@
 class Account:
     name = ''  # 公有變數
     __balance = 100  # 私有變數
+    __password = ''
+
+    def __init__(self, name, amount, password):
+        self.name = name
+        self.addBalance(amount)
+        self.__password = password
 
     def addBalance(self, amount):
         if (amount > 0):
             self.__balance = self.__balance + amount
 
+    def getBalance(self, password):
+        if (self.__password == password):
+            return self.__balance
+        else:
+            print("密碼不正確")
+
     def __str__(self):
         return self.name + ' 有 $' + str(self.__balance)
 
 if __name__ == '__main__':
-    acc1 = Account()
-    acc1.name = 'Vincent'
-    #acc1.__balance = -200
-    #acc1.addBalance(-200)
-    acc1.addBalance(200)
-    print(acc1)
+    # acc1 = Account()
+    # acc1.name = 'Vincent'
+    # #acc1.__balance = -200
+    # #acc1.addBalance(-200)
+    # acc1.addBalance(200)
+    # print(acc1)
+
+    acc2 = Account('Anita', 5000, '1234')
+    print('Balance $', acc2.getBalance('12345'))
+    print(acc2)
