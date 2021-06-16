9883e4389c2f5ba1181ad1105711932d4b3c828d
ftche <ftchen911@gmail.com>
2021-06-16 19:52:09 +0800

    python

--- day08/OO_2.py
+++ day08/OO_2.py
@@ -8,27 +8,60 @@ class Account:
         self.addBalance(amount)
         self.__password = password
 
-    def addBalance(self, amount):
+    def addBalance(self, amount):  # 存款
         if (amount > 0):
             self.__balance = self.__balance + amount
+            return True
+        else:
+            return False
 
-    def getBalance(self, password):
+    def subBalance(self, amount):  # 提款
+        if (amount > 0 and amount <= self.__balance):
+            self.__balance = self.__balance - amount
+            return True
+        else:
+            return False
+
+    def getBalance(self, password):  # 查詢餘額
         if (self.__password == password):
             return self.__balance
         else:
             print("密碼不正確")
 
+    # 轉帳
+    # def transfer(self, acct_out, acct_in, amount):
+    #     if (self.name == acct_in):
+    #         self.addBalance(amount)
+    #         if (self.name == acct_out):
+    #             self.subBalance(amount)
+
+    def transfer(self, acct, amount):
+        status = self.subBalance(amount)  # 提出
+        if (status):  # if (status == True)
+            acct.addBalance(amount)  # 存入
+
+
+
     def __str__(self):
         return self.name + ' 有 $' + str(self.__balance)
 
 if __name__ == '__main__':
-    # acc1 = Account()
-    # acc1.name = 'Vincent'
+     # acc1 = Account()
+     # acc1.name = 'Vincent'
     # #acc1.__balance = -200
     # #acc1.addBalance(-200)
-    # acc1.addBalance(200)
+     #acc1.addBalance(200)
     # print(acc1)
 
-    acc2 = Account('Anita', 5000, '1234')
-    print('Balance $', acc2.getBalance('12345'))
-    print(acc2)
+    # acc2 = Account('Anita', 5000, '1234')
+    # print('Balance $', acc2.getBalance('12345'))
+    #acc2.subBalance(700)
+    #print('Balance $', acc2.getBalance('12345'))
+    # acc2.transfer('Anita', 'Vincent', 1000)
+    # print(acc2)
+
+    a = Account('Anita', 5000, '1234')
+    b = Account('Bob', 1000, '5678')
+    print(a, b)
+    a.transfer(b, 1500)
+    print(a,b)
