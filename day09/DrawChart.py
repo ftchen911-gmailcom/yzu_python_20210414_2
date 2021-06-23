3ec60bf7484518f699fa51e53da07907af5d5fff
ftche <ftchen911@gmail.com>
2021-06-23 21:27:16 +0800

    python

---
+++ day09/DrawChart.py
@@ -0,0 +1,21 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#conn = sqlite3.connect('demo.db')
+#df = pd.read_sql_query("select * from Stock order by id")
+
+conn = sqlite3.connect('demo.db')
+df = pd.read_sql_query("select * from Stock order by id", con=conn)
+print(df)
+
+# 繪圖
+ma = df['price'].rolling(window=2).mean()   # window=2 二點計算一個平均
+print(ma)
+plt.plot(df['id'], df['price']) # 繪製折線圖
+plt.plot(df['id'], ma)  # 繪製移動平均線
+plt.xlabel('day')
+plt.ylabel('price')
+plt.show()
+
+conn.close()
